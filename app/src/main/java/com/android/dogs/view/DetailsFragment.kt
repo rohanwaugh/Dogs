bf70bb1b8343d33c7b50ec11e01a95a19286d447
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-10 19:18:47 +0530

    Code cleanup

--- app/src/main/java/com/android/dogs/view/DetailsFragment.kt
+++ app/src/main/java/com/android/dogs/view/DetailsFragment.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.android.dogs.R
 
 /**
@@ -13,12 +12,22 @@ import com.android.dogs.R
  */
 class DetailsFragment : Fragment() {
 
+    private var dogUuid = 0
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            dogUuid = DetailsFragmentArgs.fromBundle(it).dogUuid
+        }
     }
 
 }
--- app/src/main/java/com/android/dogs/view/ListFragment.kt
+++ app/src/main/java/com/android/dogs/view/ListFragment.kt
@@ -5,8 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
 import com.android.dogs.R
+
 
 /**
  * A simple [Fragment] subclass.
@@ -20,5 +20,4 @@ class ListFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
-
 }
