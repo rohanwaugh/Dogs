534b03daeaa214e10aef4229f0f2b2518bfa1d36
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-10 17:21:39 +0530

    1. Project basic package structure created. 2. Navigation setup

---
+++ app/src/main/java/com/android/dogs/view/DetailsFragment.kt
@@ -0,0 +1,24 @@
+package com.android.dogs.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.android.dogs.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class DetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+}
--- app/src/main/java/com/android/dogs/view/MainActivity.kt
+++ app/src/main/java/com/android/dogs/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.android.dogs
+package com.android.dogs.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.dogs.R
 
 class MainActivity : AppCompatActivity() {
 
