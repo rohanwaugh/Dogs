9ab6d0c0a884494560f2f445b33463f1aeea832b
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-21 22:19:06 +0530

    Recyclerview adapter implementation

---
+++ app/src/main/java/com/android/dogs/view/DogsAdapter.kt
@@ -0,0 +1,40 @@
+package com.android.dogs.view
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.android.dogs.R
+import com.android.dogs.model.DogBreed
+import kotlinx.android.synthetic.main.item_dog.view.*
+
+class DogsAdapter(private val dogsList:ArrayList<DogBreed>): RecyclerView.Adapter<DogsAdapter.DogsViewHolder>() {
+
+    fun updateDogsList(newDogsList: List<DogBreed>){
+        dogsList.clear()
+        dogsList.addAll(newDogsList)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DogsViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_dog,parent,false)
+        return DogsViewHolder(view)
+    }
+
+    override fun getItemCount() = dogsList.count()
+
+    override fun onBindViewHolder(holder: DogsViewHolder, position: Int) {
+        holder.bindDog(dogsList[position])
+    }
+
+
+
+    inner class DogsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bindDog(dogBreed: DogBreed) {
+            itemView.dogName.text = dogBreed.dogBreed
+            itemView.lifeSpan.text = dogBreed.lifeSpan
+        }
+
+    }
+}
