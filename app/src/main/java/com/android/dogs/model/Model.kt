7fa71bf0b108c3bb0423d32346daf7f6d116afdd
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-21 21:57:13 +0530

    Model class created

---
+++ app/src/main/java/com/android/dogs/model/Model.kt
@@ -0,0 +1,11 @@
+package com.android.dogs.model
+
+data class DogBreed(
+    val breedId: String?,
+    val dogBreed: String?,
+    val lifeSpan: String?,
+    val breedGroup: String?,
+    val breedFor: String?,
+    val temperament: String?,
+    val imageUrl: String?
+)
