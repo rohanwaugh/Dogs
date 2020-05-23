9e1cba35eefba4d36136c73d138317d59123c7b6
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-23 20:00:51 +0530

    Dao and Room database implementation

---
+++ app/src/main/java/com/android/dogs/room/DogDao.kt
@@ -0,0 +1,22 @@
+package com.android.dogs.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.android.dogs.model.DogBreed
+
+@Dao
+interface DogDao {
+
+    @Insert
+    suspend fun insertAll(vararg dogs: DogBreed): List<Long>
+
+    @Query("SELECT * FROM dogbreed")
+    suspend fun getAllDogs(): List<DogBreed>
+
+    @Query("SELECT * FROM dogbreed WHERE uuid = :dogId")
+    suspend fun getDog(dogId: Int): DogBreed
+
+    @Query("DELETE FROM dogbreed")
+    suspend fun deleteAllDogs()
+}
---
+++ app/src/main/java/com/android/dogs/room/DogDatabase.kt
@@ -0,0 +1,31 @@
+package com.android.dogs.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.android.dogs.model.DogBreed
+
+@Database(entities = [DogBreed::class], version = 1)
+abstract class DogDatabase : RoomDatabase() {
+
+    abstract fun dogDao(): DogDao
+
+    companion object {
+        @Volatile private var instance: DogDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context.applicationContext,
+            DogDatabase::class.java,
+            "dogdatabse"
+        ).build()
+
+    }
+}
