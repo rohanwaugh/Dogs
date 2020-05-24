04e207d85e2e31f15cdfcbefb85bf5594d37c707
Rohan Wagh <rohan.wagh@cognizant.com>
2020-05-24 14:36:38 +0530

    Palette implementation

--- app/src/main/java/com/android/dogs/view/DetailsFragment.kt
+++ app/src/main/java/com/android/dogs/view/DetailsFragment.kt
@@ -1,5 +1,7 @@
 package com.android.dogs.view
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -8,9 +10,14 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.palette.graphics.Palette
 import com.android.dogs.R
 import com.android.dogs.databinding.FragmentDetailsBinding
+import com.android.dogs.model.DogPalette
 import com.android.dogs.viewmodel.DogsDetailsViewModel
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 
 
 /**
@@ -55,8 +62,32 @@ class DetailsFragment : Fragment() {
 
             dog?.let {
                 dataBinding.dog = dog
+
+                it.imageUrl?.let { url ->
+                    setupBackgroundColor(url)
+                }
             }
         })
+    }
+
+    private fun setupBackgroundColor(url: String) {
+        Glide.with(this)
+            .asBitmap()
+            .load(url)
+            .into(object : CustomTarget<Bitmap>() {
+                override fun onLoadCleared(placeholder: Drawable?) {
+                }
+
+                override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                    Palette.from(resource)
+                        .generate { palette ->
+                            val color = palette?.vibrantSwatch?.rgb ?: 0
+                            val dogPalette = DogPalette(color)
+                            dataBinding.palette = dogPalette
+                        }
+                }
+
+            })
     }
 
 }
